4c6e50ef89cc2b114a085df37b5f72e8b1876da7
Adrian Kummerlaender <knairda@t-online.de>
2014-10-21 19:41:13 +0200

    Enabled customizing of the number of commits to be fetched

--- source/01_raw/commits.xsl
+++ source/01_raw/commits.xsl
@@ -13,10 +13,12 @@
 	<target     mode="plain" value="commits.xml"/>
 </xsl:variable>
 
+<xsl:variable name="commit_count">20</xsl:variable>
+
 <xsl:template match="repositories/entry">
 	<entry handle="{@handle}">
 		<xsl:copy-of select="InputXSLT:external-command(
-			concat('./utility/git_log.sh ', path)
+			concat('./utility/git_log.sh ', path, ' ', $commit_count)
 		)/self::command/commit"/>
 	</entry>
 </xsl:template>
--- utility/git_log.sh
+++ utility/git_log.sh
@@ -2,6 +2,6 @@
 
 format="<commit hash=\"%h\"><date>%cd</date><message><![CDATA[# %B]]></message></commit>"
 
-  git  --no-pager -C $1 log --date=iso --pretty=tformat:"$format"  \
-| tidy --input-xml yes --escape-cdata true --wrap 0                \
+  git  --no-pager -C $1 log -n $2 --date=iso --pretty=tformat:"$format"  \
+| tidy --input-xml yes --escape-cdata true --wrap 0                      \
 | sed  -e 's~^\([\*]\)\{3\}~\t\t\*~g' -e 's~^\([\*]\)\{2\}~\t\*~g'
